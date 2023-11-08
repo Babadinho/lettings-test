cf7529394c9675f15740067adc1feb2d2b99632c
Donald Iloekwe <donytex4u@gmail.com>
2023-11-08 00:10:40 +0000

    add logout functionality

--- tasker.js
+++ tasker.js
@@ -1,4 +1,11 @@
+const logoutButton = document.querySelector('.logout');
+
 const storedUserName = localStorage.getItem('userName');
 if (!storedUserName) {
   window.location.href = 'index.html';
 }
+
+logoutButton.addEventListener('click', () => {
+  localStorage.removeItem('userName');
+  window.location.href = 'index.html';
+});
